634c571fb33c23a5071c88e76ec7e4bf4f8130fa
Mehedi Hasan Shihab <sshihabb007@gmail.com>
2019-04-24 13:48:04 +0600

    Add files via upload

---
+++ DBAccess/Connection.cs
@@ -0,0 +1,24 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAccess
+{
+    public class Connection
+    {
+        static string strConnectionString = "Data Source=XE;User Id=hospital2;Password=hospital2;";
+        OracleConnection objConn;
+
+        public OracleConnection DB()
+        {
+            if (objConn == null)
+            {
+                objConn = new OracleConnection(strConnectionString);
+            }
+            return objConn;
+        }
+    }
+}
